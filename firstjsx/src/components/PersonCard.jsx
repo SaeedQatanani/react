d9a94eede595a7121f043ef487ecfaf2d34548c6
SaeedQatanani <saeedqatananil@gmail.com>
2022-12-07 15:48:01 +0200

    putting it together

--- firstjsx/src/components/PersonCard.jsx
+++ firstjsx/src/components/PersonCard.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            counter: this.props.age,
+        };
+    }
+    increseCounter = () => {
+        this.setState({ counter: this.state.counter+1 });
+    }
     render() {
         return (
             <div>
                 <h1>{ this.props.firstName }, { this.props.lastName }</h1>
-                <p>Age: { this.props.age }</p>
+                <p>Age: { this.state.counter }</p>
                 <p>Hair Color: { this.props.hairColor }</p>
+                <button onClick={ this.increseCounter }>Birthday Button for { this.props.firstName } { this.props.lastName }</button>
             </div>
         );
     }
